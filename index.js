46096296bc478a3b53480306c6059716104d765a
Kateryna Strough <katerynastrough@hotmail.com>
2020-01-08 23:10:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ class Formatter {
   static capitalize( string ) {
     return string.charAt(0).toUpperCase()+string.slice(1)
   }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
 }
